0aef47f394eb3259a48dbd67094944affcad031b
Matt Ammirati <matt.ammirati@gmail.com>
2022-06-04 20:52:06 -1000

    built sign in

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,22 @@
 describe('spec.cy.js', () => {
-  it('should visit', () => {
+  //Arrange
+  //Act
+  //Assertion
+  it('should visit our sign in page', () => {
     cy.visit('http://localhost:3000/')
-    cy.contains('Hello Cypress!')
+    cy.contains('Sign In').click()
+  })
+
+  it('should allow input', () => {
+    cy.get('.uname-input')
+      .type('hermanmuenster@gmail.com').should('have.value', 'hermanmuenster@gmail.com')
+    cy.get('.pword-input')
+      .type('pass1234').should('have.value', 'pass1234')
+  })
+
+  it('should throw an error if username is not in the database', () => {
+    cy.get('.submit-button').click()
+    cy.contains('Invalid Username')
+
   })
 })
--- src/App.js
+++ src/App.js
@@ -1,9 +1,77 @@
 import './App.css';
+import React, { useState } from "react"
+import ReactDOM  from 'react-dom';
 
 function App() {
+  const [errorMessages, setErrorMessages] = useState({})
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ]
+
+  const errors = {
+    uname: "Invalid Username",
+    pword: "Invalid Password"
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    var {uname , pword} = document.forms[0]
+
+    const userData = database.find((user) => user.username === uname.value)
+
+    if(userData) {
+      if(userData !== pword.value){
+        setErrorMessages({name: "pword", message: errors.pword})
+      } else {
+        setIsSubmitted(true)
+      }
+    } else {
+      setErrorMessages({name: "uname", message: errors.uname})
+    }
+  }
+
+  const renderErrorMessages = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    )
+
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="inputContainer">
+          <label>Username: </label>
+          <input className="uname-input" type="text" name="uname" required />
+          {renderErrorMessages("uname")}
+        </div>
+        <div className="inputContainer">
+          <label>Password: </label>
+          <input className="pword-input" type="text" name="pword" required />
+          {renderErrorMessages("pname")}
+        </div>
+        <div className="buttonContainer">
+          <input className="submit-button" type="submit" />
+        </div>
+      </form>
+    </div>
+  )
+
+
   return (
     <div className="App">
-      <h1 style={{ textAlign: 'center'}}>Hello Cypress!</h1>
+      <div className="login-form">
+        <div className="title">Sign In</div>
+        {isSubmitted ? <div> User is Successfully Logged In</div> : renderForm}
+      </div>
     </div>
   );
 }
